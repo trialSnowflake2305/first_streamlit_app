1891b70848b5ea5d7bb40c89ee6b9be90adaee04
trialSnowflake2305 <132303463+trialSnowflake2305@users.noreply.github.com>
2023-05-01 16:53:54 -0400

    Update streamlit_app.py
    minor edits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omega 3 & Blueberry oatmeal")
 streamlit.text("🥗 Kale, Spinach and Rocket smoothie")
 streamlit.text("🐔 Hard-boiled Free-range Eggs")
 streamlit.text("🥑🍞 Avocado Toast")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
