4422c7b029128ca02a05110bbfec2ca6a39b4858
trialSnowflake2305 <132303463+trialSnowflake2305@users.noreply.github.com>
2023-05-01 16:58:21 -0400

    Update streamlit_app.py
    added the list of fruits from an S3 bucket.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit 
+import pandas
+
 streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omega 3 & Blueberry oatmeal")
 streamlit.text("🥗 Kale, Spinach and Rocket smoothie")
 streamlit.text("🐔 Hard-boiled Free-range Eggs")
 streamlit.text("🥑🍞 Avocado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
