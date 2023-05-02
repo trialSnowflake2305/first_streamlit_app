e75e5cad306adcd5031ebb050bd556962d4d84fa
trialSnowflake2305 <132303463+trialSnowflake2305@users.noreply.github.com>
2023-05-02 10:00:15 -0400

    Update streamlit_app.py
    another test while working on the bug.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,12 +26,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT * FROM fruit_load_list")
-#my_data_rows = my_cur.fetchall()
-#streamlit.text("Fruit load list conatins:")
-#streamlit.dataframe(my_data_rows)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * FROM fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.text("Fruit load list conatins:")
+streamlit.dataframe(my_data_rows)
 
 
 fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
