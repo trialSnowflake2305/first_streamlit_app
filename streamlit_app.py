6ee10e4fda0ff0c665514a0ea869fda97e709468
trialSnowflake2305 <132303463+trialSnowflake2305@users.noreply.github.com>
2023-05-02 11:13:58 -0400

    Update streamlit_app.py
    now user can add fruit to SF.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ streamlit.text("🥑🍞 Avocado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
@@ -22,12 +23,12 @@ streamlit.dataframe(fruits_to_show)
 
 
 
+streamlit.header("Fruityvice Fruit Advice!")
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
-streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
@@ -38,6 +39,7 @@ except URLErros as e:
     streamlit.error()
 
 
+    
 streamlit.text("Fruit load list conatins:")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -50,7 +52,17 @@ if streamlit.button("Get fruit load list"):
     streamlit.dataframe(my_data_rows)
 
 
-streamlit.stop()
 
-fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.text("Thanks for adding " + fruit_choice + "!")
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("INSERT INTO fruit_load_list VALUES (" + new_fruit + ")" )
+        return "Thanks for adding " + new_fruit + "!"
+    
+if streamlit.button("Add a new fruit"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    streamlit.text(insert_row_snowflake(add_my_fruit))
+    
+
+streamlit.stop()
