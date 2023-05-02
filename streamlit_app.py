3a3295b3074781abe6ff248a2dd51a32f3b83bef
trialSnowflake2305 <132303463+trialSnowflake2305@users.noreply.github.com>
2023-05-02 09:20:45 -0400

    Update streamlit_app.py
    added some code to test connection with SF.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,11 @@ streamlit.text("🐔 Hard-boiled Free-range Eggs")
 streamlit.text("🥑🍞 Avocado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 
@@ -27,3 +24,11 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
