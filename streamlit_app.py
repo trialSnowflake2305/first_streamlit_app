d26c97ae1822bd6e40dcf97b00dd2d48c22c4ab0
trialSnowflake2305 <132303463+trialSnowflake2305@users.noreply.github.com>
2023-05-02 10:31:00 -0400

    Update streamlit_app.py
    some adjutsments;

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit 
 import pandas
 import requests
+from urllib.error import URLError
 import snowflake.connector
+
 
 streamlit.title("My Mom's New Healthy Diner!")
 streamlit.header("Breakfast Favorites")
@@ -17,6 +19,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+streamlit.stop()
 
 
 streamlit.header("Fruityvice Fruit Advice!")
