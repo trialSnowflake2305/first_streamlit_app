c3274ca005b659f2a0dcbef83a68d94627796c04
trialSnowflake2305 <132303463+trialSnowflake2305@users.noreply.github.com>
2023-05-01 21:52:36 -0400

    Update streamlit_app.py
    enhanced the API call.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
